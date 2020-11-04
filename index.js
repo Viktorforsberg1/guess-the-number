64fdca31f9d87d52bb9b855a60c6ae4eebfe9356
Viktor Forsberg <viktor.forsberg@skola.botkyrka.se>
2020-11-04 11:44:02 +0100

    Första delen

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Read instructions. And remove this line.
+const secretNumber = randomInt()
+
+function randomInt(){
+    return Math.floor(Math.random() * 100) + 1
+}
+
+function getUserGuess(){
+    const stringValue = document.getElementById("user-input").value
+    return parseInt(stringValue, 10)
+}
+
+
+document.addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+        
+        if (secretNumber === getUserGuess()){
+            setMessage('CORRECT!!!')
+        }
+
+         else if (secretNumber > getUserGuess()) {
+            setMessage('Number too low!')
+        }
+
+         else if (secretNumber < getUserGuess()) {
+            setMessage('Number too high!')
+        }
+        
+        document.getElementById('user-input').value = ''
+    }
+})
+
+function setMessage(msg){
+    document.getElementById("message").innerText = msg
+}
